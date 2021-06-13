8ad72745177689a58cbe267a27c0c7ca3d662247
Raul Oliva <oliva.raul12@gmail.com>
2021-06-12 22:21:14 -0500

    Adding ejs template to display the current host

--- api/api.js
+++ api/api.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const http = require('../http/http')
-
 // enabling cross origin CORS-Policy
 router.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const app = express()
 const port = process.env.PORT || 5000
 const router = require('./api/api')
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/views`))
+app.set('view engine', 'ejs')
+
+// renders docs about the api
+app.get('/', (req, res) => {
+    res.render('index', { host: `${req.protocol}://${req.get('host')}` })
+})
 
 app.use(router)
 
 app.listen(port, console.log(`server started at port: ${port}`))
-
-// renders docs about the api
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`)
-})
 
 module.exports = app
